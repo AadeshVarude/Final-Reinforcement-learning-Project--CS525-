edf6643bdc7fee1776aea87f5a17186f00958aa8
AadeshVarude <50541542+AadeshVarude@users.noreply.github.com>
2022-12-05 19:33:25 -0500

    Add files via upload

---
+++ DQN/argument.py
@@ -0,0 +1,17 @@
+def add_arguments(parser):
+    '''
+    Add your arguments here if needed. The TA will run test.py to load
+    your default arguments.
+
+    For example:
+        parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
+        parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
+    '''
+    # parser.add_argument('--EPSILON', type=float, default=1.0, help='batch size for training')
+    # parser.add_argument('--EPSILON_END', type=float, default=0.025, help='batch size for training')
+    # parser.add_argument('--EPSILON_DECAY', type=float, default=1.0, help='batch size for training')
+    # parser.add_argument('--DECAY_EPSILON_AFTER', type=int, default=3000, help='batch size for training')
+    # parser.add_argument('--TARGET_UPDATE_FREQUENCY', type=int, default=1000, help='batch size for training')
+    # parser.add_argument('--BUFFER_SIZE', type=int, default=1000 ,help='batch size for training')
+    
+    return parser
