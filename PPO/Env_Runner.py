03e9fb66fd37212d3943d49fca7c78e1f1f8111b
Om Gaikwad <98101801+omg0809@users.noreply.github.com>
2022-12-05 19:46:34 -0500

    Add files via upload

---
+++ PPO/Env_Runner.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from collections import deque
+import numpy as np
+# import wandb
+# wandb.init(project='PPO SPACE-INVADERS',entity='omg0809')
+
+device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dtype = torch.float
+epi_reward=deque([0.0],maxlen=100)
+
+class Logger:
+    
+    def __init__(self, filename):
+        self.filename = filename
+        
+        f = open(f"{self.filename}.csv", "w")
+        f.close()
+        
+    def log(self, msg):
+        f = open(f"{self.filename}.csv", "a+")
+        f.write(f"{msg}\n")
+        f.close()
+
+cur_step = 0 
+episode = 0          
+class Env_Runner:
+    
+    def __init__(self, env, agent, logger_folder):
+        super().__init__()
+        
+        self.env = env
+        self.agent = agent
+        
+        self.logger = Logger(f'{logger_folder}/training_info')
+        self.logger.log("training_step, return")
+        
+        self.ob = self.env.reset()
+        
+    def run(self, steps):
+        
+        global cur_step
+        global episode
+        
+        obs = []
+        actions = []
+        rewards = []
+        dones = []
+        values = []
+        action_prob = []
+        
+        for step in range(steps):
+            
+            self.ob = torch.tensor(self.ob).to(device).to(dtype)
+            policy, value = self.agent(self.ob.unsqueeze(0))
+            action = self.agent.select_action(policy.detach().cpu().numpy()[0])
+            
+            obs.append(self.ob)
+            actions.append(action)
+            values.append(value.detach())
+            action_prob.append(policy[0,action].detach())
+            
+            self.ob, r, done, info, additional_done = self.env.step(action)
+                      
+            if done:
+                # print("Done")
+                self.ob = self.env.reset()
+                episode+=1
+                if "return" in info:
+                    #print(" Found the return",episode)
+                    epi_reward.append(info['return'])
+                    # wandb.log({' return per step :' :info["return"],'Number of steps taken':cur_step+step})
+                    self.logger.log(f'{cur_step+step},{info["return"]}')
+                    if episode%100==0:
+                        print('Yes',episode)
+                        # wandb.log({'reward per 100 episodes:' : np.mean(epi_reward)})
+
+            
+            rewards.append(r)
+            dones.append(done or additional_done)
+            
+        cur_step += steps
+                                    
+        return [obs, actions, rewards, dones, values, action_prob]
